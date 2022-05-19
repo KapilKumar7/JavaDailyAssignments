32ea0c1234205ac64bed1a35b3af2ba3f8214b47
KapilKumar7 <100344844+KapilKumar7@users.noreply.github.com>
2022-05-19 22:45:34 +0530

    Add files via upload
    demoday4

---
+++ Week1/src/com/masai/Demo.java
@@ -0,0 +1,38 @@
+package com.masai;
+
+public class Demo {
+static	String s="String";
+static int i=5;
+static float f =20;
+	
+	Demo(){
+		this(s);
+		System.out.println("Inside Demo()");
+		
+		
+	};
+	
+	Demo(String s){
+		this(i);
+		System.out.println("Inside Demo(String s)"+s);
+			
+		}
+	Demo(int i){
+		this(f);
+		System.out.println("Inside Demo(int i)" +i);
+		
+	}
+	Demo(float f){
+		System.out.println("Inside Demo(float f)"+f);
+		
+	}
+
+	
+
+	public static void main(String[] args) {
+		
+		Demo c1 = new Demo();
+
+	}
+
+}
